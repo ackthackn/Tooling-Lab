c5732f2f1a3a26b6d616604e8314668a91c8518c
Ethan <white2e9@mail.uc.edu>
2024-03-21 19:56:20 -0400

    idk man

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -19,47 +19,47 @@ resetGameButton.classList.add(`d-none`);
 
 // updateScoreTallyUI
 function updateScoreTallyUI() {
-   scoreParagraph.innerHTML = `${game.username}: ${game.score.user} v CPU: ${game.score.cpu}`;
+  scoreParagraph.innerHTML = `${game.username}: ${game.score.user} v CPU: ${game.score.cpu}`;
 }
 
 // updateGameHistoryUI
 function updateGameHistoryUI() {
-   gameHistoryParagraph.innerHTML = ``;
-   gameHistoryParagraph.innerHTML = game.gameHistoryLog;
+  gameHistoryParagraph.innerHTML = ``;
+  gameHistoryParagraph.innerHTML = game.gameHistoryLog;
 }
 
 // start-game-button EventListener
 startGameButton.addEventListener(`click`, function (e) {
-   e.preventDefault();
-   username = userName.value;
-   game = new RockPaperScissors(username);
+  e.preventDefault();
+  username = userName.value;
+  game = new RockPaperScissorss(username);
 
-   welcomeScreen.classList.add(`d-none`);
-   gameScreen.classList.remove(`d-none`);
-   resetGameButton.classList.remove(`d-none`);
-   updateScoreTallyUI();
-   updateGameHistoryUI();
-   // Complete
+  welcomeScreen.classList.add(`d-none`);
+  gameScreen.classList.remove(`d-none`);
+  resetGameButton.classList.remove(`d-none`);
+  updateScoreTallyUI();
+  updateGameHistoryUI();
+  // Complete
 });
 
 // go-button EventListener
 goButton.addEventListener(`click`, function (e) {
-   e.preventDefault();
-   const selectedOption = userSelection.options[userSelection.selectedIndex].value;
-   game.play(userSelection.value);
-   updateScoreTallyUI();
-   updateGameHistoryUI();
+  e.preventDefault();
+  const selectedOption = userSelection.options[userSelection.selectedIndex].value;
+  game.play(userSelection.value);
+  updateScoreTallyUI();
+  updateGameHistoryUI();
 
 });
 
 //reset-game-button
 resetGameButton.addEventListener(`click`, function (e) {
-   e.preventDefault();
-   userName.value = ``;
-   username = ``;
-   game = new RockPaperScissors(username);
+  e.preventDefault();
+  userName.value = ``;
+  username = ``;
+  game = new RockPaperScissors(username);
 
-   welcomeScreen.classList.remove(`d-none`);
-   gameScreen.classList.add(`d-none`);
-   resetGameButton.classList.add(`d-none`);
+  welcomeScreen.classList.remove(`d-none`);
+  gameScreen.classList.add(`d-none`);
+  resetGameButton.classList.add(`d-none`);
 })

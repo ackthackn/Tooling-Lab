db9cafaf37f06da3e77dcaa18116d8e725e6161a
Ethan <white2e9@mail.uc.edu>
2024-03-21 20:28:36 -0400

    created first test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,14 @@
+/* eslint-disable no-undef */
 describe(`My First Test`, () => {
-  it(`clicks the link "type"`, () => {
+  it(`clicking "type" navigates to a new url`, () => {
     cy.visit(`https://example.cypress.io`)
 
     cy.contains(`type`).click()
+
+    cy.url().should(`include`, `/commands/actions`)
+
+    cy.get(`.action-email`).type(`fake@email.com`)
+
+    cy.get(`.action-email`).should(`have.value`, `fake@email.com`)
   })
 })

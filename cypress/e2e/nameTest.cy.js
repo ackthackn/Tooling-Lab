5aa2e63e62c0abe662537c97ca3aff91f3964927
Ethan <white2e9@mail.uc.edu>
2024-03-21 21:25:17 -0400

    worked on the test file

--- cypress/e2e/nameTest.cy.js
+++ cypress/e2e/nameTest.cy.js
@@ -1,4 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
+
+Cypress.on(`uncaught:exception`, (err, runnable) => {
+  return false;
+})
+
 describe(`template spec`, () => {
   it(`types name, selects dropdown choice, confirms history log`, () => {
     cy.visit(`index.html`)
@@ -6,5 +12,7 @@ describe(`template spec`, () => {
     cy.get(`input[name="username"]`).type(`Ethan White`)
 
     cy.get(`select`).select(`rock`)
+
+    cy.get(`button[id="go-button"`).click()
   })
 })
